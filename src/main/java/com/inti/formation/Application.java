61971f6ff5171c177165f9b024118706d364219c
BlueWorldInc <keamybakunin@gmail.com>
2019-10-12 21:40:36 +0200

    Le message du commit est obligatoire même avec une signature.
    Signed-off-by: BlueWorldInc <keamybakunin@gmail.com>

--- src/main/java/com/inti/formation/Application.java
+++ src/main/java/com/inti/formation/Application.java
@@ -29,7 +29,6 @@ public class Application {
 		System.out.println("je suis là2");
 		System.out.println("ici la terre github me recoit il ?");
 		System.out.println("ici la terre github me recoit il ?");
-		System.out.println("ici la terre github me recoit il ?");
 		
 	}
 
